a187e655e19110d231f711f4145073c455a4d3ab
Rushi Agrawal <rushi.agr@gmail.com>
2012-11-01 05:16:38 +0530

    Finished profiles feature. Happy :)

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 import os
 import sys
+import pickle
+
 from gi.repository import Gtk
 
 from credentials import Credentials
@@ -28,6 +30,36 @@ class DestructionPopUp(PopUp):
     def on_click_ok(self, widget):
         sys.exit()
 
+class SavePopup:
+    """
+    Class for popup which appears and asks for the name by which one want to
+    save the profile as.
+    """
+    def __init__(self, profiles, default_save_name=''):
+        """ Profiles is list of profiles to pickle"""
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file('save.glade')
+        dic = {
+            'onclick_save_button': self.onclick_save_button,
+            'onclick_cancel_button': self.onclick_cancel_button,
+        }
+        self.builder.connect_signals(dic)
+        self.builder.get_object('save_textbox').set_text(default_save_name)
+        self.profiles_list = profiles
+        
+    def onclick_save_button(self, widget):
+        last_correct_element = {
+            'name': self.builder.get_object('save_textbox').get_text(),
+            'value': self.profiles_list[-1]
+        }
+        self.profiles_list[-1] = last_correct_element
+        profile_file = open('profile.dat', 'wb')
+        pickle.dump(self.profiles_list,profile_file)
+        self.builder.get_object('main_window').destroy()
+        return 
+    
+    def onclick_cancel_button(self, widget):
+        self.builder.get_object('main_window').destroy()
 
 class MainWindow:
     """
@@ -40,11 +72,31 @@ class MainWindow:
         self.obj("ftp_grid").set_sensitive(False)
         self.popup = Gtk.Builder()
         self.popup.add_from_file("popup.glade")
+
+        profile_file = open('profile.dat', 'rb')
+        file_data = profile_file.readlines()
+        profile_file.close()
+
+        profile_file = open('profile.dat', 'rb')
+        
+        if file_data[0] == '\n':
+            self.saved_profiles = []
+        else:
+            self.saved_profiles = pickle.load(profile_file)
+        
+        profile_file.close()
+
+        for profile in self.saved_profiles:
+            self.obj('comboboxtext1').append_text(profile['name'])
         
         dic = { 
             "onclick_quit_window": self.quit,
+            
             "onclick_cancel_button": self.quit,
             "onclick_apply_button": self.onclick_apply_button,
+            "onclick_load_button": self.onclick_load_button,
+            "onclick_save_button": self.onclick_save_button,
+
             "onclick_not_use_proxy_radio": self.onclick_not_use_proxy_radio,
             "onclick_use_proxy_radio": self.onclick_use_proxy_radio,
             "toggle_use_same_proxy_checkbox":
@@ -88,14 +140,18 @@ class MainWindow:
             self.obj(minibox_type + elem).set_sensitive(sensitivity)
             
 
-    def onclick_apply_button(self, widget):
+    def get_data(self):
+        """
+        Takes all the data from the user interface and bundles it into a
+        dictionary, and returns it
+        """
         data = {}
         if self.obj('not_use_proxy_radio').get_active() == True:
             data['noproxy'] = True
         else:
             data['noproxy'] = False
 
-            http = {
+            data['http'] = {
                 'proxy': self.obj('http_proxy_textbox').get_text(),
                 'port': self.obj('http_port_textbox').get_text(),
                 'user': self.obj('http_user_textbox').get_text(),
@@ -109,7 +165,7 @@ class MainWindow:
             else:
                 data['sameproxy'] = False
 
-                https = {
+                data['https'] = {
                     'proxy': self.obj('https_proxy_textbox').get_text(),
                     'port': self.obj('https_port_textbox').get_text(),
                     'user': self.obj('https_user_textbox').get_text(),
@@ -118,7 +174,7 @@ class MainWindow:
                             .get_active() is True else False,
                 }
                 
-                ftp = {
+                data['ftp'] = {
                     'proxy': self.obj('ftp_proxy_textbox').get_text(),
                     'port': self.obj('ftp_port_textbox').get_text(),
                     'user': self.obj('ftp_user_textbox').get_text(),
@@ -127,16 +183,45 @@ class MainWindow:
                             .get_active() is True else False,
                 }
                 
+        return data
 
-                data['https'] = https
-                data['ftp'] = ftp
-
-
-            data['http'] = http
+    def set_data(self, data):
+        """
+        Takes the dictionary and fills the values in the UI accordingly
+        """
+        if data['noproxy'] is True:
+            self.obj('not_use_proxy_radio').set_active(True)
+            self.obj('all_values_box').set_sensitive(False)
+        else:
+            self.obj('not_use_proxy_radio').set_active(False)
+            self.obj('use_proxy_radio').set_active(True)
+            self.obj('all_values_box').set_sensitive(True)
+            if data['sameproxy'] is True:
+                self.obj('use_same_proxy_checkbox').set_active(True)
+                proxies = ['http']
+                self.obj('https_grid').set_sensitive(False)
+                self.obj('ftp_grid').set_sensitive(False)
+            else:
+                self.obj('use_same_proxy_checkbox').set_active(False)
+                proxies = ['http', 'https', 'ftp']
+                self.obj('https_grid').set_sensitive(True)
+                self.obj('ftp_grid').set_sensitive(True)
+            for proxy in proxies:
+                for label in ['proxy', 'port', 'user', 'password']:
+                    self.obj(proxy+'_'+label+'_textbox').set_text(data[proxy][label])
+                if data[proxy]['use_auth'] is False:
+                    self.minibox_sensitivity(False, proxy)
+                else:
+                    self.minibox_sensitivity(True, proxy)
         
-        print data
         
-        cred = Credentials(data)
+
+    def onclick_apply_button(self, widget):
+        
+        dat = self.get_data()
+#        print dat
+        
+        cred = Credentials(dat)
         errors = cred.validate()
         
         if len(errors) is not 0:
@@ -153,6 +238,44 @@ class MainWindow:
             popup.popup_text('Proxy applied to the system! :)')
             popup.builder.get_object('main_window').show_all()
 
+    def onclick_save_button(self, widget):
+        #TODO(rushiagr): check if all the values from all fields are valid
+        
+        #NOTE(rushiagr): here, the last element of profile_list is the
+        #       dictionary returned by function get_data. This last element
+        #       will be modified in SavePopup class.
+        
+        profile_file = open('profile.dat', 'rb')
+        file_data = profile_file.readlines()
+        profile_file.close()
+        profile_file = open('profile.dat', 'rb')
+        
+        if file_data[0] == '\n':
+            profile_list = []
+        else:
+            profile_list = pickle.load(profile_file)
+        
+        profile_file.close()
+        profile_list.append(self.get_data())
+        
+        
+        default_profile_name = 'profile'+str(len(profile_list))
+        save_popup = SavePopup(profile_list, default_profile_name)
+        save_popup.builder.get_object('main_window').show_all()
+        
+        
+        pass
+    
+    def onclick_load_button(self, widget):
+#        print self.saved_profiles
+#        self.set_data(self.saved_profiles)
+        selected = self.obj('comboboxtext1').get_active_text()
+#        print selected
+        if selected is not None:
+            for profile in self.saved_profiles:
+                if profile['name'] == selected:
+                    self.set_data(profile['value'])
+        pass
     
     def onclick_not_use_proxy_radio(self, widget):
         for proxy_type in ['http', 'https', 'ftp']:
